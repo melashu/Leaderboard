5be3917dc50d2e1993f3166030bf945a3bef355c
melashu <meshu102@outlook.com>
2022-08-25 12:03:00 +0300

    Add event listner

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-import '../css/style.css';
+import "../css/style.css";
+import leaderboardAPI from "../module/APIImp";
+import { displayAllScore, displayEachScore } from "../module/UI";
+
+
+document
+  .querySelector(".form-add-score")
+  .addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const name = document.querySelector(".name").value;
+    const score = document.querySelector(".score").value;
+
+    await leaderboardAPI.addScore({
+      user: name,
+      score: score,
+    });
+    displayEachScore({
+      user: name,
+      score: score,
+    });
+  });
+
+document.addEventListener("DOMContentLoaded", async (e) => {
+  const result = await leaderboardAPI.getScore();
+  displayAllScore(result);
+});
+
+document.querySelector(".refresh").addEventListener("click", async (e) => {
+  const table = document.querySelector(".score-table");
+  table.innerHTML = '';
+  const result = await leaderboardAPI.getScore();
+  displayAllScore(result);
+});
