b4a69aa4d51eab4ad0656bd0e2c7e8626ffcb342
melashu <meshu102@outlook.com>
2023-01-04 23:46:47 +0300

    Add one td inside table row

--- module/UI.js
+++ module/UI.js
@@ -1,16 +1,20 @@
+const table = document.querySelector(".score-table");
+
 const displayAllScore = (allScore) => {
-    for (const score of allScore) {
+  for (const score of allScore) {
     displayEachScore(score);
-    }
+  }
 };
 
 const displayEachScore = async (score) => {
-  const table = document.querySelector(".score-table");
   const tr = document.createElement("tr");
   const td = document.createElement("td");
-  td.innerHTML = `${score["user"]}  ${score["score"]}`;
+  const td1 = document.createElement("td");
+  td.innerHTML = `${score["user"]}`;
+  td1.innerHTML = `${score["score"]}`;
   tr.appendChild(td);
+  tr.appendChild(td1);
   table.appendChild(tr);
 };
 
-export  { displayAllScore, displayEachScore };
+export { displayAllScore, displayEachScore };
